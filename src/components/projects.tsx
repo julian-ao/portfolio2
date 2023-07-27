f3ee6ffbc6073cabedbb1703f611f4f2143a8e33
Julian <jotto0214@gmail.com>
2023-07-27 21:48:22 +0200

    Small changes

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,11 +3,12 @@ import { Project } from "./project";
 
 export const Projects = () => {
   return (
-    <div id="projects" className="flex flex-col items-center mb-60 gap-52">
+    <div
+      id="projects"
+      className="flex flex-col items-center pt-20 mb-60 gap-52"
+    >
       {projects.map((project, index) => {
-        return (
-          <Project key={index} {...project} reverse={index % 2 === 1} />
-        );
+        return <Project key={index} {...project} reverse={index % 2 === 1} />;
       })}
     </div>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,17 @@
 import { ProjectProps } from "./components/project";
 
-export  const hiText = "Hi, I'm Julian 👋"
+export const hiText = "Hi, I'm Julian 👋";
 
 export const magicText = [
   "Frontend developer",
   "Informatics student",
   "Software developer",
-]
+];
 export const aboutText = [
   {
     title: "Introduction",
     icon: "rocket",
-    text:   "My name is Julian Ammouche Ottosen, I'm a passionate software developer who loves programming. Over the years, I've embraced coding, constantly seeking new challenges to grow my skills.",
+    text: "My name is Julian Ammouche Ottosen, I'm a passionate software developer who loves programming. Over the years, I've embraced coding, constantly seeking new challenges to grow my skills.",
   }, // With a keen eye for detail and a strong commitment to high-quality software, I'm thrilled to share my journey and projects with you.
   // Often products becomes more complex than needed due to the fast pace of startups. Simplifying and redesigning the user experience can make the product more effective.
   {
@@ -26,37 +26,48 @@ export const aboutText = [
   }, // As a software developer, I bring valuable traits to the table. I excel in collaboration, effective communication, and constructive contributions within diverse teams.With an insatiable thirst for knowledge, I continuously seek opportunities for learning and self-improvement, staying at the forefront of cutting-edge developments in the software industry.
 ];
 
-export const projects= [
+export const projects = [
   {
-    title: 'Enua installer app',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    technologies: ['React Native', 'TypeScript', 'Scrum', 'Figma'],
-    images: ['./src/assets/images/iphone.png', './src/assets/images/iphone-screen.png'],
-    typeOfImages: 'mobile',
-    github: '',
+    title: "Enua installer app",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    technologies: ["React Native", "TypeScript", "Scrum", "Figma"],
+    images: [
+      "./src/assets/images/iphone.png",
+      "./src/assets/images/iphone-screen.png",
+    ],
+    typeOfImages: "mobile",
+    github: "",
   } as ProjectProps,
   {
-    title: 'Previous portfolio',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    technologies: ['React', 'TailwindCSS', 'TailwindCSS'],
-    images: ['./src/assets/images/portfolio1Skills.jpg'],
-    typeOfImages: 'desktop',
-    github: '',
+    title: "Previous portfolio",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    technologies: ["React", "TailwindCSS", "TailwindCSS"],
+    images: ["./src/assets/images/portfolio1Skills.jpg"],
+    typeOfImages: "desktop",
+    github: "",
   } as ProjectProps,
   {
-    title: 'Previous portfolio',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    technologies: ['React', 'TailwindCSS', 'TailwindCSS'],
-    images: ['./src/assets/images/portfolio1Skills.jpg'],
-    typeOfImages: 'desktop',
-    github: '',
+    title: "Password manager",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    technologies: ["React", "TailwindCSS", "TailwindCSS"],
+    images: ["./src/assets/images/portfolio1Skills.jpg"],
+    typeOfImages: "desktop",
+    github: "",
   } as ProjectProps,
   {
-    title: 'Enua installer app',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    technologies: ['React Native', 'TypeScript', 'Scrum', 'Figma'],
-    images: ['./src/assets/images/iphone.png', './src/assets/images/iphone-screen.png'],
-    typeOfImages: 'mobile',
-    github: '',
+    title: "Enua installer app",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    technologies: ["React Native", "TypeScript", "Scrum", "Figma"],
+    images: [
+      "./src/assets/images/iphone.png",
+      "./src/assets/images/iphone-screen.png",
+      "./src/assets/images/iphone.png",
+    ],
+    typeOfImages: "mobile",
+    github: "",
   } as ProjectProps,
-]
+];
