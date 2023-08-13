52761d7ae23fc007770f76b79372b07f15e94597
Julian <jotto0214@gmail.com>
2023-08-13 11:50:29 +0200

    Min-height on button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -5,12 +5,14 @@ interface ButtonProps {
   onPress: () => void;
 }
 
-export const Button: preact.FunctionComponent<ButtonProps> = ({ children, onPress }) => {
-
+export const Button: preact.FunctionComponent<ButtonProps> = ({
+  children,
+  onPress,
+}) => {
   return (
     <div
       className="bg-white/[.75] flex justify-center items-center rounded drop-shadow-[0_4px_4px_rgba(0,0,0,0.05)] font-medium cursor-pointer duration-200 hover:bg-gray-100 select-none backdrop-blur tracking-wider
-      text-base sm:text-lg w-44 sm:w-52 h-10 sm:h-12"
+      text-base sm:text-lg w-44 sm:w-52 h-10 sm:h-12 min-h-[2.5rem]"
       onClick={onPress}
     >
       {children}
