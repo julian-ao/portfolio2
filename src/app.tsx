d44fbcbb99117ceece7b7a69c906de061bafc04c
Julian <julianao@stud.ntnu.no>
2024-05-01 12:04:28 +0200

    Installed vercel web anayltics

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,12 @@ import { Background } from "./components/background";
 import { Intro } from "./components/intro";
 import { Navbar } from "./components/navbar";
 import { Projects } from "./components/projects";
+import { Analytics } from "@vercel/analytics/react"
 
 export const App = () => {
   return (
     <Background>
+      <Analytics/>
       <Navbar />
       <Intro />
       <Projects />
