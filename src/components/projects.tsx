844475f4c3161b4a26930608deb8ab18c04f66a5
Julian <julianao@stud.ntnu.no>
2024-05-10 13:38:27 +0200

    Fix build error

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,11 +6,19 @@ export const Projects = () => {
   return (
     <div
       id="projects"
-      className="flex flex-col items-center pt-20 mb-32 md:mb-60
-      gap-32 sm:gap-40 xl:gap-64"
+      className="flex flex-col items-center pt-20 mb-32 md:mb-60 gap-32 sm:gap-40 xl:gap-64"
     >
       {projects.map((project, index) => {
-        return <Project key={index} {...project} reverse={index % 2 === 1} />;
+        if (!project) return null
+        const { title = "Untitled Project", ...rest } = project;
+        return (
+          <Project
+            key={index}
+            title={title}
+            {...rest}
+            reverse={index % 2 === 1}
+          />
+        );
       })}
       <Button onPress={() => window.open("https://github.com/julian-ao", "_blank")}>
         View more
